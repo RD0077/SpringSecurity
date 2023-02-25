1188f7c92eef81eb94ebfc5a1fe3270cef9b5d25
ASUS <rahul.dhotkar143@gmail.com>
2023-02-25 15:18:11 +0530

    Spring Sec with JDBC

--- SpringSecurityWithJDBC/src/main/java/com/study/security/config/SecConfig.java
+++ SpringSecurityWithJDBC/src/main/java/com/study/security/config/SecConfig.java
@@ -1,5 +1,61 @@
 package com.study.security.config;
 
-public class SecConfig {
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication()
+//		.dataSource(dataSource)
+//		.withDefaultSchema()
+//		.withUser(User.withUsername("user")
+//				.password("pass")
+//				.roles("USER"))
+//		.withUser(User.withUsername("admin")
+//				.password("admin")
+//				.roles("ADMIN"));
+//	}
+	
+	//Default DB as per spring docs
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication().dataSource(dataSource);
+//	}
+	
+	//if DB names are changed
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.jdbcAuthentication().dataSource(dataSource)
+	.usersByUsernameQuery("select username,password,enabled from users where username = ?")
+	.authoritiesByUsernameQuery("select username,authority from authorities where usernae = ?");
+	
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().antMatchers("/admin").hasRole("ADMIN").antMatchers("/user")
+				.hasAnyRole("ADMIN", "USER").antMatchers("/").permitAll().and().formLogin();
+	}
+	@Bean
+	public PasswordEncoder getPasswordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 }
--- SpringSecurityWithJDBC/src/main/java/com/study/security/controller/MyController.java
+++ SpringSecurityWithJDBC/src/main/java/com/study/security/controller/MyController.java
@@ -1,5 +1,24 @@
 package com.study.security.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class MyController {
 
+	@GetMapping("/")
+	public String home()
+	{
+		return ("<h1> Welcome Home</h1>");
+	}
+	@GetMapping("/admin")
+	public String admin()
+	{
+		return ("<h1> Welcome Admin</h1>");
+	}
+	@GetMapping("/user")
+	public String user()
+	{
+		return ("<h1> Welcome user</h1>");
+	}
 }
